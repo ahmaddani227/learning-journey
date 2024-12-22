601950c797fe3401c6d553d9fadc987e23b0d75e
ahmaddani227 <putraahmaddani833@gmail.com>
2024-12-22 19:14:00 +0700

    feat(nodejs): add basic NodeJS Web Server

---
+++ NodeJS/basic-nodejs/nodejs-web-server/app.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const fs = require("fs");
+
+const port = 3000;
+
+const renderHTML = (path, res) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.write("Error: file not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+};
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    const url = req.url;
+
+    switch (url) {
+      case "/about":
+        renderHTML("./about.html", res);
+        break;
+      case "/contact":
+        renderHTML("./contact.html", res);
+        break;
+      default:
+        renderHTML("./index.html", res);
+        break;
+    }
+
+    // if (url === "/about") {
+    //   //   res.write("<h1>Ini adalah halaman about</h1>");
+    //   renderHTML("./about.html", res);
+    // } else if (url === "/contact") {
+    //   renderHTML("./contact.html", res);
+    // } else {
+    //   //   res.write("Hello world");
+    //   renderHTML("./index.html", res);
+    // }
+  })
+  .listen(port, () => {
+    console.log("Server is Listenig on port 3000....");
+  });
