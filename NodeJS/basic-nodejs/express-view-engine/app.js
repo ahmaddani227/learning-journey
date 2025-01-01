8a58e1f569ce3c66df4e672150253aef1b161561
ahmaddani227 <putraahmaddani833@gmail.com>
2025-01-01 10:37:11 +0700

    feat(nodejs): add basic Express View Engine

---
+++ NodeJS/basic-nodejs/express-view-engine/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
+const port = 3000;
+
+// EJS
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+
+app.get("/", (req, res) => {
+  // res.sendFile("./index.html", { root: __dirname });
+  res.render("index", {
+    layout: "layouts/main-layout",
+    title: "Halaman Home",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    layout: "layouts/main-layout",
+    title: "Halaman About",
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    layout: "layouts/main-layout",
+    title: "Halaman Contact",
+  });
+});
+
+app.get("/product/:id", (req, res) => {
+  res.send(`Produk id : ${req.params.id} <br> Category: ${req.query.category}`);
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>404</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
